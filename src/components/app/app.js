58230ee3352179564c3f320fe271dbba00784c70
revuwem <revuwem@gmail.com>
2020-09-16 15:37:40 +0300

    set routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
-import ErrorBoundry from '../error-boundry';
-import ErrorButton from '../error-button';
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+import Header from '../header';
+import {
+    HomePage,
+    ShopPage,
+    CartPage
+} from '../pages';
 
 import './app.css';
 
 const App = () => {
 
-    return(
-        <ErrorBoundry>
-            <ErrorButton />
-        </ErrorBoundry>
+    return (
+        <div className="bookcase-store">
+            <Header />
+
+
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/shop/" component={ShopPage} />
+                <Route path="/cart" component={CartPage} />
+
+                <Redirect to="/" />
+            </Switch>
+
+        </div>
     );
 }
 
