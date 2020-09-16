49c07b29b329ef30f07d69932b903bf0d47e9eca
revuwem <revuwem@gmail.com>
2020-09-16 14:28:22 +0300

    base app structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import App from './components/app';
+import ErrorBoundry from './components/error-boundry';
+import BookcaseService from './services/bookcase-service';
+import { BookcaseServiceProvider} from './components/bookcase-service-context';
 
+import store from './store';
 
+const bookcaseService = new BookcaseService();
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <ErrorBoundry>
+            <BookcaseServiceProvider value={bookcaseService}>
+                <Router>
+                    <App/>
+                </Router>
+            </BookcaseServiceProvider>
+        </ErrorBoundry>
+    </Provider>,
     document.getElementById('root')
 );
