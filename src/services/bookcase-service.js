0ee37b639835285a9bc325d17f57c34b58669458
revuwem <revuwem@gmail.com>
2020-09-16 14:36:30 +0300

    dummy data added

--- src/services/bookcase-service.js
+++ src/services/bookcase-service.js
@@ -1,7 +1,23 @@
 
-export default class BookcaseService{
+export default class BookcaseService {
 
-    getBooks(){
-        return [];
+    getBooks() {
+        return [
+            {
+                id: 1,
+                title: 'Production-Ready Microservices',
+                author: 'Susan J. Fowler'
+            },
+            {
+                id: 2, 
+                title: 'Code Complete',
+                author: 'Steven C. McConnell'
+            },
+            {
+                id: 3,
+                title: 'Release It!',
+                author: 'Michael T. Nygard'
+            }
+        ];
     }
 }
