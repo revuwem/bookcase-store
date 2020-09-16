f040fb5f920d6941a091b5924d374795675fd06a
revuwem <revuwem@gmail.com>
2020-09-16 11:25:41 +0300

    component created

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-rolling-vql8i03dnc">
+            <div class="ldio-wmntysyu5wh">
+                <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;
