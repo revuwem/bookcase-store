c30dc48160a3fb09d7199b03545e5c5cb3be6409
revuwem <revuwem@gmail.com>
2020-09-21 11:57:40 +0300

    created store enhancers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,43 @@
-import {createStore} from 'redux';
+import { createStore, compose } from 'redux';
 
-import {reducer} from './reducers';
+import { reducer } from './reducers';
 
-const store = createStore(reducer);
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            });
+        }
+
+        originalDispatch(action);
+    };
+
+    return store;
+};
+
+const logEnhancer = (createStore) => (...args) =>{
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+
+    store.dispatch = (action) => {
+        console.log(action.type);
+        originalDispatch(action);
+    };
+
+    return store;
+}
+
+
+const store = createStore(reducer, compose(
+    stringEnhancer, 
+    logEnhancer)
+);
+
+
+store.dispatch('HELLO_WORLD');
 
 export default store;
