31ff3e2f5e6a4f89cb6f9b99c44f6d4f68ed281d
revuwem <revuwem@gmail.com>
2020-09-21 12:28:12 +0300

    created middlewares

--- src/store.js
+++ src/store.js
@@ -1,43 +1,59 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import { reducer } from './reducers';
 
-const stringEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
 
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return originalDispatch({
-                type: action
-            });
-        }
-
-        originalDispatch(action);
-    };
-
-    return store;
+const logMiddleware = ({getState}) => (next) => (action) => {
+    console.log(action.type, getState());
+    return next(action);
 };
 
-const logEnhancer = (createStore) => (...args) =>{
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-
-    store.dispatch = (action) => {
-        console.log(action.type);
-        originalDispatch(action);
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
     };
 
-    return store;
-}
+    return next(action);
+};
 
-
-const store = createStore(reducer, compose(
-    stringEnhancer, 
-    logEnhancer)
-);
+const store = createStore(reducer, applyMiddleware(
+    stringMiddleware,
+    logMiddleware
+));
 
 
 store.dispatch('HELLO_WORLD');
 
 export default store;
+
+
+// const stringEnhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return originalDispatch({
+//                 type: action
+//             });
+//         }
+
+//         originalDispatch(action);
+//     };
+
+//     return store;
+// };
+
+// const logEnhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const originalDispatch = store.dispatch;
+
+//     store.dispatch = (action) => {
+//         console.log(action.type);
+//         originalDispatch(action);
+//     };
+
+//     return store;
+// };
